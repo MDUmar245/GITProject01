9d9ff90d74a73bf3d4e81ff8ed08e7e9abaffd35
MDUmar245 <mdumarshia@gmail>
2023-04-15 13:59:58 +0530

    First Time Push

---
+++ src/test/java/com/git1/GITprt01.java
@@ -0,0 +1,21 @@
+package com.git1;
+
+public class GITprt01 {
+	
+	private void method() {
+		System.out.println("01");
+
+	}
+	
+	private void method2() {
+		System.out.println("02");
+
+	}
+	
+	private void method3() {
+		System.out.println("03");
+
+	}
+	
+
+}
